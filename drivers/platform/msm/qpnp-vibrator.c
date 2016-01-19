7b6c974ad089f07f494a12e5ca57039b11754353
maxwen <max.weninger@gmail.com>
2016-01-19 23:37:34 +0100

    n3: remove unneeded changes
    -remove rmt_storage memory needed for CM blob
    
    Change-Id: Ia49784ea18a69447d1f879c962563f13fe153430

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -242,11 +242,6 @@ retry:
 	if (value == 0)
 		vib->state = 0;
 	else {
-#ifdef CONFIG_OPPO_DEVICE_N3
-//Zhilong.Zhang@OnlineRd.Driver, 2014/10/23, Add for improve the effect of motor vibration
-		if (value < 50 && value > 0)
-			value += 10;
-#endif		
 		value = (value > vib->timeout ?
 				 vib->timeout : value);
 		vib->state = 1;
